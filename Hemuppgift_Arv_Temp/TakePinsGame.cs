2e7dca2db140ebccc4f7c4cedaa41e360392742a
aleksanderlukic <aleksander.lukic@edu.newton.se>
2024-10-14 21:30:30 +0200

    Jag har skapat en Main Window för spelet Take Pins Game

---
+++ Hemuppgift_Arv_Temp/TakePinsGame.cs
@@ -0,0 +1,31 @@
+﻿using Hemuppgift_Arv_Temp;
+using System;
+
+namespace Game
+{
+    class TakePinsGame
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board(10); // Starta med 10 stickor
+            Player human = new HumanPlayer("Alicent");
+            Player computer = new ComputerPlayer("Aemond");
+
+            while (!board.IsEmpty())
+            {
+                human.TakePins(board);
+                if (board.IsEmpty())
+                {
+                    Console.WriteLine($"{human.name} vinner!");
+                    break;
+                }
+
+                computer.TakePins(board);
+                if (board.IsEmpty())
+                {
+                    Console.WriteLine($"{computer.name} vinner!");
+                }
+            }
+        }
+    }
+}
