bb4afb73cff107941647f33c7e3757a83ad8af34
aleksanderlukic <aleksander.lukic@edu.newton.se>
2024-10-14 21:11:45 +0200

    Jag har nu skapat en window för playern

---
+++ Hemuppgift_Arv_Temp/Player.cs
@@ -0,0 +1,14 @@
+﻿namespace Game
+{
+    public abstract class Player
+    {
+        protected string name;
+
+        public Player(string name)
+        {
+            this.name = name;
+        }
+
+        public abstract int TakePins(Board board);
+    }
+}
